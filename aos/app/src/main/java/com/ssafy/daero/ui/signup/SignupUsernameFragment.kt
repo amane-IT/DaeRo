f2aa92d50519efec47a609e73cd753ceaa87ec4e
gyp03236 <gyp03236@lab.ssafy.com>
2022-07-26 14:02:07 +0900

    [S07P12D110-307] feat: aos: 회원가입(닉네임 입력) 기능 추가 - 닉네임 중복 검사 요청 - 이용약관 페이지에 이동 - 이용약관 체크 & 닉네임 중복 여부 통과의 경우 이동

--- aos/app/src/main/java/com/ssafy/daero/ui/signup/SignupUsernameFragment.kt
+++ aos/app/src/main/java/com/ssafy/daero/ui/signup/SignupUsernameFragment.kt
@@ -1,6 +1,12 @@
 package com.ssafy.daero.ui.signup
 
+import android.graphics.Color
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.ssafy.daero.R
 import com.ssafy.daero.base.BaseFragment
 import com.ssafy.daero.data.dto.signup.SignupNicknameRequestDto
@@ -18,25 +24,49 @@ class SignupUsernameFragment : BaseFragment<FragmentSignupUsernameBinding>(R.lay
     }
 
     private fun setOnClickListeners(){
+        var flag = false
+
         binding.apply {
             buttonSignupUsernameNextStep.setOnClickListener{
-                val dto = SignupNicknameRequestDto(editTextSignupUsernameUserName.text.toString())
-                signupUsernameViewModel.verifyNickname(dto)
+                if(buttonSignupUsernameNextStep.text.equals("닉네임 중복 검사")){
+                    Log.d("TAG", "setOnClickListeners: " + editTextSignupUsernameUserName.text.toString())
+                    val dto = SignupNicknameRequestDto(editTextSignupUsernameUserName.text.toString())
+                    signupUsernameViewModel.verifyNickname(dto)
+                    binding.buttonSignupUsernameNextStep.isEnabled = false
+                }
+                else{
+                    if(flag){
+                        findNavController().navigate(R.id.action_signupUsernameFragment_to_tripPreferenceFragment)
+                    }
+                }
+            }
+
+            textSignupUsernameEnterTerm.setOnClickListener {
+                findNavController().navigate(R.id.action_signupUsernameFragment_to_termsFragment)
+            }
+
+            checkboxSignupUsernameTermCheck.setOnCheckedChangeListener { box, isChecked ->
+                flag = isChecked
             }
         }
     }
 
     private fun observeData(){
+        signupUsernameViewModel.showProgress.observe(viewLifecycleOwner){
+            binding.progressBarSignupUsernameLoading.isVisible = it
+        }
+
         signupUsernameViewModel.responseState.observe(viewLifecycleOwner){ state ->
             when(state){
                 SUCCESS -> {
                     binding.textSignupUsernameCheckMessage.text = "사용가능한 닉네임입니다."
+                    binding.buttonSignupUsernameNextStep.text = "가입하기"
                 }
                 FAIL -> {
                     binding.textSignupUsernameCheckMessage.text = "중복된 닉네임입니다."
+                    binding.buttonSignupUsernameNextStep.isEnabled = true
                 }
             }
-
         }
     }
 }
