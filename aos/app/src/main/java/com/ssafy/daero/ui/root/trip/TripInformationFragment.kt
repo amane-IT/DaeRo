394b00b1aaaf5e467c31f6dc3d7777f441d29ef7
hsw0715 <hsw0715@naver.com>
2022-07-29 12:17:46 +0900

    design: aos: 여행 상세 페이지 UI

--- aos/app/src/main/java/com/ssafy/daero/ui/root/trip/TripInformationFragment.kt
+++ aos/app/src/main/java/com/ssafy/daero/ui/root/trip/TripInformationFragment.kt
@@ -3,9 +3,29 @@ package com.ssafy.daero.ui.root.trip
 import com.ssafy.daero.R
 import com.ssafy.daero.base.BaseFragment
 import com.ssafy.daero.databinding.FragmentTripInformationBinding
+import com.ssafy.daero.utils.view.setStatusBarOrigin
+import com.ssafy.daero.utils.view.setStatusBarTransparent
 
 class TripInformationFragment : BaseFragment<FragmentTripInformationBinding>(R.layout.fragment_trip_information) {
     override fun init() {
 
+    }
+
+    private fun setStatusBarTransParent() {
+        binding.constraintTripInformationInnerContainer.setStatusBarTransparent(requireActivity())
+    }
+
+    private fun setStatusBarOrigin() {
+        requireActivity().setStatusBarOrigin()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setStatusBarTransParent()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        setStatusBarOrigin()
     }
 }
