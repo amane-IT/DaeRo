8ddcd5edefdac64fbe0a94460ccf63e4e95caa9e
gyp03236 <gyp03236@lab.ssafy.com>
2022-07-29 16:40:22 +0900

    [S07P12D110-313] design: 트립스탬프 바텀시트 UI 구현 - 다시 찍기 버튼 클릭 시, 바텀시트 구현

--- aos/app/src/main/java/com/ssafy/daero/ui/root/trip/TripStampBottomSheetFragment.kt
+++ aos/app/src/main/java/com/ssafy/daero/ui/root/trip/TripStampBottomSheetFragment.kt
@@ -1,12 +1,53 @@
 package com.ssafy.daero.ui.root.trip
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.ssafy.daero.R
 import com.ssafy.daero.base.BaseFragment
 import com.ssafy.daero.databinding.FragmentTripStampBottomSheetBinding
 
 
-class TripStampBottomSheetFragment : BaseFragment<FragmentTripStampBottomSheetBinding>(R.layout.fragment_trip_stamp_bottom_sheet) {
-    override fun init() {
+class TripStampBottomSheetFragment(val applyFilter : (String, String) -> Unit) : BottomSheetDialogFragment() {
+    private var _binding: FragmentTripStampBottomSheetBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTripStampBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        expandFullHeight()
+    }
+
+    private fun expandFullHeight() {
+        val bottomSheet =
+            dialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+        val behavior = BottomSheetBehavior.from<View>(bottomSheet!!)
+        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun setOnClickListeners() {
+        binding.apply {
+            imageTripStampCamera.setOnClickListener {
+                // TODO: 카메라 촬영 기능
+            }
+
+            imageTripStampGallery.setOnClickListener {
+                // TODO: 갤러리 사진 선택 기능
+            }
+        }
     }
 }
