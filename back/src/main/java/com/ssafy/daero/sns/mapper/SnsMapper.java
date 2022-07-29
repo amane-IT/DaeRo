98e43636b9fb68202c92b009a4d9962210aeda03
이주영 <ph2978@naver.com>
2022-07-29 12:05:19 +0900

    [S07P12D110-286] feat: back: 댓글 조회, 생성, 수정, 삭제

--- back/src/main/java/com/ssafy/daero/sns/mapper/SnsMapper.java
+++ back/src/main/java/com/ssafy/daero/sns/mapper/SnsMapper.java
@@ -1,8 +1,11 @@
 package com.ssafy.daero.sns.mapper;
 
+import com.ssafy.daero.sns.dto.ReplyDto;
 import com.ssafy.daero.sns.vo.ArticleVo;
+import com.ssafy.daero.sns.vo.ReplyVo;
 import com.ssafy.daero.sns.vo.StampVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -21,6 +24,21 @@ public interface SnsMapper {
     Integer selectUserSeqByArticleSeq(int articleSeq);
 
     Integer deleteArticleByArticleSeq(int articleSeq);
+
+
+    ArrayList<ReplyVo> selectReplyListByArticleSeq(@Param("articleSeq") int articleSeq, @Param("page") int page);
+
+    Integer selectUserSeqByReplySeq(int replySeq);
+
+    Integer insertReply(@Param("articleSeq") int articleSeq, @Param("userSeq") int userSeq, @Param("content") String content);
+
+    int selectArticleByArticleSeq(int articleSeq);
+
+    Integer updateReplyByReplySeq(@Param("replySeq") int replySeq, @Param("content") String content);
+
+    Integer deleteReplyByReplySeq(int replySeq);
+
+
 
 
 }
