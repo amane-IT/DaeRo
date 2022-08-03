a51c5bd0fd9ad9a713cc8eb6f0f2bc19c8f92f7e
gyp03236 <gyp03236@lab.ssafy.com>
2022-08-03 15:57:47 +0900

    [S07P12D110-329] feat: aos: 컬렉션 페이지 기능 구현
    추후 해야할 것
    - API 연결하기
    - 아이템 누르면 해당 게시글/유저 페이지 이동 // 좋아요 해제 기능 구현
    - 좋아요 해제 시, 리스트에서 아이템 삭제 기능 추가

---
+++ aos/app/src/main/java/com/ssafy/daero/ui/root/collection/CollectionViewModel.kt
@@ -0,0 +1,36 @@
+package com.ssafy.daero.ui.root.collection
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.rxjava3.cachedIn
+import com.ssafy.daero.base.BaseViewModel
+import com.ssafy.daero.data.dto.collection.CollectionItem
+import com.ssafy.daero.data.repository.SnsRepository
+import com.ssafy.daero.utils.constant.FAIL
+
+class CollectionViewModel  : BaseViewModel() {
+    private val TAG = "CollectionVM_DaeRo"
+    private val snsRepository = SnsRepository.get()
+
+    val responseState = MutableLiveData<Int>()
+
+    private val _collections = MutableLiveData<PagingData<CollectionItem>>()
+    val collections: LiveData<PagingData<CollectionItem>>
+        get() = _collections
+
+    fun getCollections(){
+        addDisposable(
+            snsRepository.getCollection().cachedIn(viewModelScope)
+                .subscribe({
+                    Log.d(TAG, "getCollection: $it")
+                    _collections.postValue(it)
+                }, { throwable ->
+                    Log.d(TAG, "getCollection: ${throwable.toString()}")
+                    responseState.postValue(FAIL)
+                })
+        )
+    }
+}
