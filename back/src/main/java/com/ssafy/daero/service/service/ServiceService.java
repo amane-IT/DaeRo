d22d8ac909843f9e9414fb12429927eae71e4812
이주영 <ph2978@naver.com>
2022-08-05 14:12:31 +0900

    [S07P12D110-271] feat: back: 1:1 문의 목록 조회, 등록

--- back/src/main/java/com/ssafy/daero/service/service/ServiceService.java
+++ back/src/main/java/com/ssafy/daero/service/service/ServiceService.java
@@ -1,11 +1,13 @@
 package com.ssafy.daero.service.service;
 
 import com.ssafy.daero.service.dto.FaqDto;
+import com.ssafy.daero.service.dto.InquiryDto;
 import com.ssafy.daero.service.dto.NoticeDto;
 import com.ssafy.daero.service.mapper.ServiceMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -22,5 +24,22 @@ public class ServiceService {
     public ArrayList<FaqDto> faqList() {
         ArrayList<FaqDto> faqList = serviceMapper.selectFaqList();
         return faqList;
+    }
+
+    public Map<String, Object> inquiryList(int userSeq, int page) {
+        int totalPage = (int) Math.ceil((serviceMapper.selectInquiryCountByUserSeq(userSeq))/10.0);
+        if (totalPage == 0) { totalPage = 1; }
+        if (page > totalPage) { return null; }
+        ArrayList<InquiryDto> results = serviceMapper.selectInquiryList(userSeq, page);
+        Map<String, Object> inquiryList = new HashMap<>();
+        inquiryList.put("total_page", totalPage);
+        inquiryList.put("page", page);
+        inquiryList.put("results", results);
+        return inquiryList;
+    }
+
+    public boolean createInquiry(int userSeq, String title, String content) {
+        int inserted = serviceMapper.insertInquiry(userSeq, title, content);
+        return inserted == 1;
     }
 }
