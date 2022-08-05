e4b7934bd878ecb7e713694309baf91ad66c3ace
gyp03236 <gyp03236@lab.ssafy.com>
2022-08-05 23:33:36 +0900

    [S07P12D110-311] refactor: 여행지 선호도 조사 리스트 가져오기 & 선호도 조사 결과 보내기

--- aos/app/src/main/java/com/ssafy/daero/ui/signup/TripPreferenceFragment.kt
+++ aos/app/src/main/java/com/ssafy/daero/ui/signup/TripPreferenceFragment.kt
@@ -4,12 +4,16 @@ import android.util.Log
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.ssafy.daero.R
 import com.ssafy.daero.application.App
 import com.ssafy.daero.base.BaseFragment
 import com.ssafy.daero.data.dto.signup.TripPreferenceResponseDto
 import com.ssafy.daero.databinding.FragmentTripPreferenceBinding
 import com.ssafy.daero.ui.adapter.signup.TripPreferenceAdapter
+import com.ssafy.daero.utils.constant.FAIL
+import com.ssafy.daero.utils.constant.SUCCESS
+import com.ssafy.daero.utils.view.toast
 
 class TripPreferenceFragment : BaseFragment<FragmentTripPreferenceBinding>(R.layout.fragment_trip_preference){
 
@@ -31,6 +35,8 @@ class TripPreferenceFragment : BaseFragment<FragmentTripPreferenceBinding>(R.lay
         }
 
         binding.recyclerTripPreferenceImageList.adapter = tripPreferenceAdapter
+
+        tripPreferenceViewModel.getPreferences()
     }
 
     private fun setOnClickListener(){
@@ -42,6 +48,7 @@ class TripPreferenceFragment : BaseFragment<FragmentTripPreferenceBinding>(R.lay
                     Log.d("Trip", "setOnClickListener: ${requestList.toString()}")
                     tripPreferenceViewModel.postPreference(App.prefs.userSeq, requestList)
                     // TODO: 프래그먼트 이동
+                    findNavController().navigate(R.id.action_tripPreference_to_rootFragment)
                 }
             }
         }
@@ -52,62 +59,63 @@ class TripPreferenceFragment : BaseFragment<FragmentTripPreferenceBinding>(R.lay
             binding.progressBarTripPreferenceLoading.isVisible = it
         }
 
-//        tripPreferenceViewModel.responseState_getPreference.observe(viewLifecycleOwner){ state ->
-//            when(state){
-//                SUCCESS -> {
-//                    tripPreferenceAdapter.dataList = tripPreferenceViewModel.preferenceList
-//                }
-//
-//                FAIL -> {
-//                    toast("이미지 로딩에 실패했습니다.")
-//                }
-//            }
-//        }
+        tripPreferenceViewModel.responseState_getPreference.observe(viewLifecycleOwner){ state ->
+            when(state){
+                SUCCESS -> {
+                    tripPreferenceAdapter.dataList = tripPreferenceViewModel.preferenceList
+                }
+
+                FAIL -> {
+                    toast("이미지 로딩에 실패했습니다.")
+                }
+            }
+        }
 
         tripPreferenceViewModel.count.observe(viewLifecycleOwner){
             binding.textTripPreferenceCount.text = "$it / 5"
         }
 
-        tripPreferenceAdapter.dataList = listOf(
-            TripPreferenceResponseDto(1, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(2, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(3, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(4, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(5, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(6, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(7, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(8, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(9, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-            TripPreferenceResponseDto(10, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
-        )
+//        tripPreferenceAdapter.dataList = listOf(
+//            TripPreferenceResponseDto(1, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(2, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(3, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(4, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(5, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(6, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(7, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(8, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(9, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//            TripPreferenceResponseDto(10, "dummy", "https://unsplash.com/photos/jVT8vo04UT0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fHRyaXB8ZW58MHx8fHwxNjU4ODkyNzgy&force=true&w=1920"),
+//        )
 
         tripPreferenceAdapter.notifyDataSetChanged()
     }
 
-    private val tripPreferenceItemClickListener : (View, Int) -> Unit = { _, place_seq ->
+    private val tripPreferenceItemClickListener : (View, Int) -> Unit = { _, idx ->
 
         if(tripPreferenceViewModel.count.value!! < 5){
-            if(tripPreferenceAdapter.dataList[place_seq - 1].isSelected){
-                tripPreferenceAdapter.dataList[place_seq - 1].isSelected = false
+            Log.d("TAG", ": $idx")
+            Log.d("TAG", ": ${tripPreferenceAdapter.dataList[idx].place_seq}")
+            if(tripPreferenceAdapter.dataList[idx].isSelected){
+                tripPreferenceAdapter.dataList[idx].isSelected = false
                 tripPreferenceViewModel.minusCount()
-                result.remove(place_seq)
+                result.remove(tripPreferenceAdapter.dataList[idx].place_seq)
             } else {
-                tripPreferenceAdapter.dataList[place_seq - 1].isSelected = true
+                tripPreferenceAdapter.dataList[idx].isSelected = true
                 tripPreferenceViewModel.plusCount()
-                result.add(place_seq)
+                result.add(tripPreferenceAdapter.dataList[idx].place_seq)
                 if(tripPreferenceViewModel.count.value!! == 5)
                     binding.buttonTripPreferenceStart.isEnabled = true
             }
         } else {
-            if(tripPreferenceAdapter.dataList[place_seq - 1].isSelected) {
-                tripPreferenceAdapter.dataList[place_seq - 1].isSelected = false
+            if(tripPreferenceAdapter.dataList[idx].isSelected) {
+                tripPreferenceAdapter.dataList[idx].isSelected = false
                 tripPreferenceViewModel.minusCount()
-                result.remove(place_seq)
+                result.remove(tripPreferenceAdapter.dataList[idx].place_seq)
                 binding.buttonTripPreferenceStart.isEnabled = false
             }
         }
 
-        tripPreferenceAdapter.notifyItemChanged(place_seq - 1)
+        tripPreferenceAdapter.notifyItemChanged(idx)
     }
-
 }
