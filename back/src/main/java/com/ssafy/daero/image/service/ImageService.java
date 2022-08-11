e5b01ffc0878cfe93892f7ebacc8e9dfd85a45fe
Devillness <78412140+Devillness@users.noreply.github.com>
2022-08-11 22:36:08 +0900

    chore: back: 이미지 경로 수정

--- back/src/main/java/com/ssafy/daero/image/service/ImageService.java
+++ back/src/main/java/com/ssafy/daero/image/service/ImageService.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 @Service
 public class ImageService {
     private final String IMAGE_PATH = "/var/lib/tomcat9/webapps/upload/";
-    private final String IMAGE_URL_PREFIX = "http://i7d110.p.ssafy.io/image/download/";
+    private final String IMAGE_URL_PREFIX = "https://i7d110.p.ssafy.io/api/image/download/";
     private final String[] ALLOWED_IMAGE_MIMES = new String[]{"image/jpeg", "image/png"};
 
     public ImageVo uploadFile(MultipartFile multipartFile) {
@@ -49,6 +49,7 @@ public class ImageService {
             imageVo.setResult(ImageVo.ImageResult.SERVER_ERROR);
             return imageVo;
         }
+        System.out.println("씨발 개새끼");
         imageVo.setDownloadUrl(IMAGE_URL_PREFIX + newFile.getName());
         imageVo.setResult(ImageVo.ImageResult.SUCCESS);
         return imageVo;
