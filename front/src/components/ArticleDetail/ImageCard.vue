72d7f1418a0e3966bd15614c0a0f40cc75bb3dbe
이주영 <ph2978@naver.com>
2022-08-17 00:15:47 +0900

    feat: front: 게시글 상세 조회내 Image Swiper

--- front/src/components/ArticleDetail/ImageCard.vue
+++ front/src/components/ArticleDetail/ImageCard.vue
@@ -1,22 +1,27 @@
 <template>
-  <div>
-    <div v-for="(stamp, idx) in record.trip_stamps" :key="idx">
-      <img :src="stamp.image_url" alt="tripstamp">
-    </div>
+  <div id="card">
+    <img :src="image" alt="tripstamp">
   </div>
 </template>
 
 <script>
 export default {
-  name: "imageCard",
+  name: 'imageCard',
   props: {
-    record: Object
-  }
+    image: String
+  },
+
 }
 </script>
 
 <style scoped>
 img {
   height: 500px;
+}
+#card {
+  margin: 1rem;
+  display: inline-block;
+  padding: 5px 5px;
+  margin-right: 5px;
 }
 </style>
