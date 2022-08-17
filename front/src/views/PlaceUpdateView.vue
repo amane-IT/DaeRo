e4210aa7f281ebf417eaf0da8d11d76f8d34a4f8
이주영 <ph2978@naver.com>
2022-08-17 23:39:58 +0900

    feat: front: update, create

---
+++ front/src/views/PlaceUpdateView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'placeUpdate',
+  components : { placeForm },
+  data() {
+    return {
+      place: {
+        pk: null,
+        name: '',
+        address: '',
+        latitude: null,
+        longitude: null,
+        imageUrl: '',
+        description: '',
+      },
+      action: 'UPDATE',
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
