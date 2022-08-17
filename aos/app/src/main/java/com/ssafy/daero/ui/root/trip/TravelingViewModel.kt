774e69a9f6c30f0d2f675e0813db6583d081f16e
hsw0715 <hsw0715@naver.com>
2022-08-18 02:01:51 +0900

    feat: aos: 모든 여행지 추천에 로딩 추가

--- aos/app/src/main/java/com/ssafy/daero/ui/root/trip/TravelingViewModel.kt
+++ aos/app/src/main/java/com/ssafy/daero/ui/root/trip/TravelingViewModel.kt
@@ -9,12 +9,15 @@ import com.ssafy.daero.data.dto.trip.TripInformationResponseDto
 import com.ssafy.daero.data.entity.TripStamp
 import com.ssafy.daero.data.repository.TripRepository
 import com.ssafy.daero.utils.constant.FAIL
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class TravelingViewModel : BaseViewModel() {
     private val tripRepository = TripRepository.get()
 
     val responseState = MutableLiveData<Int>()
-    var articleTripStampData = listOf<TripStamp>()
 
     private val _tripStamps = MutableLiveData<List<TripStamp>>()
     val tripStamps: LiveData<List<TripStamp>>
@@ -29,6 +32,15 @@ class TravelingViewModel : BaseViewModel() {
     var placeSeq = MutableLiveData<Int>()
     var imageUrl = MutableLiveData<String>()
     var tripRecommendState = MutableLiveData<Int>()
+
+    private val _showProgress = MutableLiveData<Boolean>()
+    val showProgress: LiveData<Boolean>
+        get() = _showProgress
+
+    // 1초 딜레이
+    private val delay =
+        Single.just(1).delay(1000, TimeUnit.MILLISECONDS).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
 
     fun getTripStamps() {
         addDisposable(
@@ -53,7 +65,7 @@ class TravelingViewModel : BaseViewModel() {
                             response.body()
                         )
                     },
-                    { throwable ->
+                    { _ ->
                         tripInformationState.postValue(FAIL)
                     }
                 )
@@ -65,49 +77,69 @@ class TravelingViewModel : BaseViewModel() {
             tripRepository.deleteAllTripStamps()
                 .subscribe({
 
-                }, { throwable ->
+                }, { _ ->
                 })
         )
         addDisposable(
             tripRepository.deleteAllTripFollow()
                 .subscribe({
 
-                }, { throwable ->
+                }, { _ ->
                 })
         )
     }
 
     fun getFirstTripRecommend(firstTripRecommend: FirstTripRecommendRequestDto) {
+        _showProgress.postValue(true)
+
         addDisposable(
             tripRepository.getFirstTripRecommend(firstTripRecommend)
-                .subscribe({ response ->
+                .flatMap { response ->
                     if (response.body()!!.place_seq != 0) {
-                        placeSeq.postValue(response.body()!!.place_seq)
                         imageUrl.postValue(response.body()!!.image_url)
-                    } else {
-                        tripRecommendState.postValue(FAIL)
                     }
-                }, { throwable ->
-                    tripRecommendState.postValue(FAIL)
-                })
+                    Single.just(response.body()!!.place_seq).delay(1000, TimeUnit.MILLISECONDS)
+                }.subscribe(
+                    { place_seq ->
+                        if (place_seq > 0) {
+                            placeSeq.postValue(place_seq)
+                        } else {
+                            tripRecommendState.postValue(FAIL)
+                        }
+                        _showProgress.postValue(false)
+                    }, { _ ->
+                        tripRecommendState.postValue(FAIL)
+                        _showProgress.postValue(false)
+                    }
+                )
         )
     }
 
     fun recommendNextPlace(time: Int, transportation: String) {
+        _showProgress.postValue(true)
+
         addDisposable(
             tripRepository.recommendNextPlace(
                 App.prefs.curPlaceSeq,
                 time, transportation
-            ).subscribe({ response ->
+            ).flatMap { response ->
                 if (response.body()!!.place_seq != 0) {
-                    placeSeq.postValue(response.body()!!.place_seq)
                     imageUrl.postValue(response.body()!!.image_url)
-                } else {
-                    tripRecommendState.postValue(FAIL)
                 }
-            }, { throwable ->
-                tripRecommendState.postValue(FAIL)
-            })
+                Single.just(response.body()!!.place_seq).delay(1000, TimeUnit.MILLISECONDS)
+            }.subscribe(
+                { place_seq ->
+                    if (place_seq > 0) {
+                        placeSeq.postValue(place_seq)
+                    } else {
+                        tripRecommendState.postValue(FAIL)
+                    }
+                    _showProgress.postValue(false)
+                }, { _ ->
+                    tripRecommendState.postValue(FAIL)
+                    _showProgress.postValue(false)
+                }
+            )
         )
     }
 }
